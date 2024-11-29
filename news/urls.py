fa663362ada6147084b20b653712a70b9571714a
Lleanin <Fifashf@yandex.ru>
2024-11-29 07:00:39 +0100

    fix bug

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('news_list/', views.NewsListView.as_view(), name='news_list'),  # Для списка новостей
-    path('<slug:slug>/', views.NewsDetailView.as_view(), name='news_detail')
+    path('news/<slug:slug>/', views.NewsDetailView.as_view(), name='news_detail')
 ]
