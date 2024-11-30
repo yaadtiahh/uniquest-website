eecd60951de76bfb83ac08100055ab14cc873771
Lleanin <Fifashf@yandex.ru>
2024-12-01 00:55:44 +0100

    Fix bugs

--- news/models.py
+++ news/models.py
@@ -7,7 +7,11 @@ class News(models.Model):
     description = models.TextField(blank=True, verbose_name='Текст новости:')
     news_image = models.ImageField(upload_to='news-imgs', null=True, verbose_name='Изображение для новости')
     content_image = models.ImageField(upload_to='content_imgs', null=True, verbose_name='Фон для блока новости')
-    published_date = models.DateField(auto_now_add=True, verbose_name='Дата публикации')
+    published_date = models.DateField(
+        auto_now_add=True, 
+        verbose_name='Дата публикации'
+    )
+    is_visible = models.BooleanField(default=True, verbose_name="Показывать новость")
 
     def __str__(self):
         return self.title
--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('news_list/', views.news_list, name='news_list'),
     path('news/<int:pk>/', views.news_detail, name='news_detail'),
     path('news/', views.news_list, name='news'),
