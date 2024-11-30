94740e183f6c7fcfa7bed7bedc3743a773a8a3b5
Lleanin <Fifashf@yandex.ru>
2024-12-01 00:58:27 +0100

    Fix bugs

--- news/views.py
+++ news/views.py
@@ -13,5 +13,5 @@ def news_detail(request, pk):
 
 
 def news_list(request):
-    news_list = News.objects.filter(is_visible=True).order_by('-created_at')
+    news_list = News.objects.filter(is_visible=True).order_by('-published_date')
     return render(request, 'news_list.html', {'news_list': news_list})
