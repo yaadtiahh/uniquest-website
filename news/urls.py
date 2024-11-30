8d227082d6d01af2a1cbb7cb72566304da89b3b4
Lleanin <Fifashf@yandex.ru>
2024-11-30 21:23:37 +0100

    fix bug

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('news/<int:pk>/', views.news_detail, name='news_detail'),
-    path('news/', views.news, name='news_list'),
+    path('news_list/', views.info_center, name='news_list'),
 ]
