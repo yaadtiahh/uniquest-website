86b74714ab025a647abaadff17af7a12e85e26ad
Lleanin <Fifashf@yandex.ru>
2024-12-01 01:40:03 +0100

    Fix bugs

--- uniquest/settings.py
+++ uniquest/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'uniquest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'news/templates')],  # Это должно быть настроено, если у вас есть глобальные шаблоны
+        'DIRS': [os.path.join(BASE_DIR, 'news', 'templates')],  # Абсолютный путь к шаблонам
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
