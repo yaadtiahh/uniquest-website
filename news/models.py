070dd1675ac7afcf5c9edb9b6b1d5836b2e711cd
Lleanin <Fifashf@yandex.ru>
2024-12-01 03:40:25 +0100

    Fix bugs

--- news/models.py
+++ news/models.py
@@ -3,8 +3,8 @@ from ckeditor.fields import RichTextField
 
 
 class News(models.Model):
-    title = RichTextField(help_text="Вы можете использовать HTML или стилизовать текст с разными цветами.")
-    description = models.TextField(blank=True, verbose_name='Текст новости:')
+    title = models.TextField(help_text="Вы можете использовать HTML или стилизовать текст с разными цветами.")
+    description = RichTextField(blank=True, verbose_name='Текст новости:')
     news_image = models.ImageField(upload_to='news-imgs', null=True, verbose_name='Изображение для новости')
     content_image = models.ImageField(upload_to='content_imgs', null=True, verbose_name='Фон для блока новости')
     published_date = models.DateField(
